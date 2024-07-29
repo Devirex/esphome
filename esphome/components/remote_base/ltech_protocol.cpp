65a15903e0e8d956ead233d440859e76b9d8e42c
Devirex <naibaf_11@yahoo.de>
2024-07-29 10:36:52 +0200

    Added Ltech

--- esphome/components/remote_base/ltech_protocol.cpp
+++ esphome/components/remote_base/ltech_protocol.cpp
@@ -37,7 +37,7 @@ optional<LTECHData> LTECHProtocol::decode(RemoteReceiveData src) {
       .data = 0,
       .nbits = 0,
   };
-  ESP_LOGI(TAG, "Received Header" + src);
+  ESP_LOGI(TAG, "Received Header %s", src );
   if (!src.expect_item(HEADER_MARK_US, HEADER_SPACE_US))
     return {};
 
