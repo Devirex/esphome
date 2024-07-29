8c23092fc3a27dde71aaf3f3a46847c9660c3a76
Devirex <naibaf_11@yahoo.de>
2024-07-29 20:55:58 +0200

    Added Ltech

--- esphome/components/remote_base/ltech_protocol.cpp
+++ esphome/components/remote_base/ltech_protocol.cpp
@@ -55,12 +55,23 @@ optional<LTECHData> LTECHProtocol::decode(RemoteReceiveData src) {
     uint8_t idx = out.nbits/8;
     uint8_t mask = out.nbits%8;
     if (src.expect_item(BIT_ONE_HIGH_US, BIT_ONE_LOW_US)) {
-      buffer[idx] = (buffer[idx] << 1) | 1;
+      if(out.nbits < 32) out.address |= 1UL << (out.nbits - 1);
+      else if(out.nbits < 40) out.mode |= 1UL << (out.nbits - 32);
+      else if(out.nbits < 64) out.rgb |= 1UL << (out.nbits - 40);
+      else if(out.nbits < 72) out.function |= 1UL << (out.nbits - 64);
+      else if(out.nbits < 80) out.white |= 1UL << (out.nbits - 72);
+      else if(out.nbits < 88) out.speed |= 1UL << (out.nbits - 80);
+      else out.crc |= 1UL << (out.nbits - 88);
     } else if (src.expect_item(BIT_ZERO_HIGH_US, BIT_ZERO_LOW_US)) {
-      buffer[idx] = (buffer[idx] << 1) | 0;
+      if(out.nbits < 32) out.address &= ~(1UL << (out.nbits - 1);
+      else if(out.nbits < 40) out.mode &= ~(1UL << (out.nbits - 32));
+      else if(out.nbits < 64) out.rgb &= ~(1UL << (out.nbits - 40));
+      else if(out.nbits < 72) out.function &= ~(1UL << (out.nbits - 64));
+      else if(out.nbits < 80) out.white &= ~(1UL << (out.nbits - 72));
+      else if(out.nbits < 88) out.speed &= ~(1UL << (out.nbits - 80));
+      else out.crc &= ~(1UL << (out.nbits - 88));
     } else if (src.expect_mark(FOOTER_MARK_US)) {
       ESP_LOGD(TAG, "Detected Footer Mark");
-      memcpy(&out, buffer, sizeof(buffer));
       return out;
     } else {
       return {};
