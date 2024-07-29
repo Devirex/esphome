65f524c35a4001ca0735e49f35d24bb8813c0a62
Devirex <naibaf_11@yahoo.de>
2024-07-29 10:54:25 +0200

    Added Ltech

--- esphome/components/remote_base/ltech_protocol.cpp
+++ esphome/components/remote_base/ltech_protocol.cpp
@@ -7,13 +7,13 @@ namespace remote_base {
 static const char *const TAG = "remote.ltech";
 
 static const int32_t TICK_US = 315;
-static const int32_t HEADER_HIGH_US = 1 * TICK_US;
-static const int32_t HEADER_LOW_US = 3.8 * TICK_US;
-static const int32_t BIT_ONE_HIGH_US = 2 * TICK_US;
-static const int32_t BIT_ONE_LOW_US = 0.9 * TICK_US;
-static const int32_t BIT_ZERO_HIGH_US = 1 * TICK_US;
-static const int32_t BIT_ZERO_LOW_US = 1.8 * TICK_US;
-static const int32_t FOOTER_MARK_US = 3.8 * TICK_US;
+static const int32_t HEADER_HIGH_US = 315;
+static const int32_t HEADER_LOW_US = 1197;
+static const int32_t BIT_ONE_HIGH_US = 630;
+static const int32_t BIT_ONE_LOW_US = 283;
+static const int32_t BIT_ZERO_HIGH_US = 315;
+static const int32_t BIT_ZERO_LOW_US = 567;
+static const int32_t FOOTER_MARK_US = 1197;
 
 void LTECHProtocol::encode(RemoteTransmitData *dst, const LTECHData &data) {
   dst->set_carrier_frequency(38000);
@@ -39,6 +39,7 @@ optional<LTECHData> LTECHProtocol::decode(RemoteReceiveData src) {
   ESP_LOGI(TAG, "Received Header");
   if (!src.expect_item(HEADER_HIGH_US, HEADER_LOW_US))
     return {};
+
   ESP_LOGI(TAG, "Detected Header");
   
   for (out.nbits = 0; out.nbits < 208; out.nbits++) {
