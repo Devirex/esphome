8eb3e689e8f8c05bbb32999c4871ed25b0baf326
Devirex <naibaf_11@yahoo.de>
2024-07-29 10:49:54 +0200

    Added Ltech

--- esphome/components/remote_base/ltech_protocol.cpp
+++ esphome/components/remote_base/ltech_protocol.cpp
@@ -7,25 +7,25 @@ namespace remote_base {
 static const char *const TAG = "remote.ltech";
 
 static const int32_t TICK_US = 315;
-static const int32_t HEADER_MARK_US = 1 * TICK_US;
-static const int32_t HEADER_SPACE_US = -3.8 * TICK_US;
+static const int32_t HEADER_HIGH_US = 1 * TICK_US;
+static const int32_t HEADER_LOW_US = 3.8 * TICK_US;
 static const int32_t BIT_ONE_HIGH_US = 2 * TICK_US;
-static const int32_t BIT_ONE_SPACE_US = -0.9 * TICK_US;
+static const int32_t BIT_ONE_LOW_US = 0.9 * TICK_US;
 static const int32_t BIT_ZERO_HIGH_US = 1 * TICK_US;
-static const int32_t BIT_ZERO_SPACE_US = -1.8 * TICK_US;
+static const int32_t BIT_ZERO_LOW_US = 1.8 * TICK_US;
 static const int32_t FOOTER_MARK_US = 3.8 * TICK_US;
 
 void LTECHProtocol::encode(RemoteTransmitData *dst, const LTECHData &data) {
   dst->set_carrier_frequency(38000);
   dst->reserve(2 + data.nbits * 2u);
 
-  dst->item(HEADER_MARK_US, HEADER_SPACE_US);
+  dst->item(HEADER_HIGH_US, HEADER_LOW_US);
 
   for (uint32_t mask = 1UL << (data.nbits - 1); mask != 0; mask >>= 1) {
     if (data.data & mask) {
-      dst->item(BIT_ONE_HIGH_US, BIT_ONE_SPACE_US);
+      dst->item(BIT_ONE_HIGH_US, BIT_ONE_LOW_US);
     } else {
-      dst->item(BIT_ZERO_HIGH_US, BIT_ZERO_SPACE_US);
+      dst->item(BIT_ZERO_HIGH_US, BIT_ZERO_LOW_US);
     }
   }
 
@@ -37,14 +37,14 @@ optional<LTECHData> LTECHProtocol::decode(RemoteReceiveData src) {
       .nbits = 0,
   };
   ESP_LOGI(TAG, "Received Header");
-  if (!src.expect_item(HEADER_MARK_US, HEADER_SPACE_US))
+  if (!src.expect_item(HEADER_HIGH_US, HEADER_LOW_US))
     return {};
   ESP_LOGI(TAG, "Detected Header");
   
   for (out.nbits = 0; out.nbits < 208; out.nbits++) {
-    if (src.expect_item(BIT_ONE_HIGH_US, BIT_ONE_SPACE_US)) {
+    if (src.expect_item(BIT_ONE_HIGH_US, BIT_ONE_LOW_US)) {
       out.data = (out.data << 1) | 1;
-    } else if (src.expect_item(BIT_ZERO_HIGH_US, BIT_ZERO_SPACE_US)) {
+    } else if (src.expect_item(BIT_ZERO_HIGH_US, BIT_ZERO_LOW_US)) {
       out.data = (out.data << 1) | 0;
     } else if (out.nbits == 208) {
       return out;
