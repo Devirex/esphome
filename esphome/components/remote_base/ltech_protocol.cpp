99e5e38b443d6666164a7f79b90985c0d5e63069
Devirex <naibaf_11@yahoo.de>
2024-07-29 21:40:37 +0200

    Added Ltech

--- esphome/components/remote_base/ltech_protocol.cpp
+++ esphome/components/remote_base/ltech_protocol.cpp
@@ -14,35 +14,6 @@ static const int32_t BIT_ZERO_HIGH_US = 315;
 static const int32_t BIT_ZERO_LOW_US = 567;
 static const int32_t FOOTER_MARK_US = 1197;
 
-uint8_t reverseBits(uint8_t byte) {
-    uint8_t reversed = 0;
-    for (int i = 0; i < 8; ++i) {
-        if (byte & (1 << i)) {
-            reversed |= (1 << (7 - i));
-        }
-    }
-    return reversed;
-}
-
-// Generische Funktion zum Umkehren der Bits in 8-Bit-Schritten
-template <typename T>
-T rev8Bit(T value) {
-    
-    // Erzeuge einen neuen Wert, der die umgekehrten 8-Bit-Blöcke enthält
-    T reversedValue = 0;
-    int numBytes = sizeof(T);
-
-    // Umkehre die Bits jedes einzelnen Bytes
-    for (int i = 0; i < numBytes; ++i) {
-        uint8_t currentByte = (value >> (i * 8)) & 0xFF;
-        uint8_t reversedByte = reverseBits(currentByte);
-        reversedValue |= (static_cast<T>(reversedByte) << (i * 8));
-    }
-
-    return reversedValue;
-}
-
-
 void LTECHProtocol::encode(RemoteTransmitData *dst, const LTECHData &data) {
   dst->set_carrier_frequency(38000);
   dst->reserve(2 + data.nbits * 2u);
@@ -79,35 +50,41 @@ optional<LTECHData> LTECHProtocol::decode(RemoteReceiveData src) {
   }
   ESP_LOGD(TAG, "Detected Header Mark");
   
+  uint8_t byte = 0;
   for (out.nbits = 0; out.nbits < 104; out.nbits++) {
     if (src.expect_item(BIT_ONE_HIGH_US, BIT_ONE_LOW_US)) {
-      if(out.nbits < 32) out.address = (out.address << 1) | 1UL;
-      else if(out.nbits < 40) out.mode = (out.mode << 1) | 1UL;
-      else if(out.nbits < 64) out.rgb = (out.rgb << 1) | 1UL;
-      else if(out.nbits < 72) out.function = (out.function << 1) | 1UL;
-      else if(out.nbits < 80) out.white = (out.white << 1) | 1UL;
-      else if(out.nbits < 88) out.speed = (out.speed << 1) | 1UL;
-      else out.crc = (out.crc << 1) | 1UL;
+      byte = (byte << 1) | 1;
     } else if (src.expect_item(BIT_ZERO_HIGH_US, BIT_ZERO_LOW_US)) {
-      if(out.nbits < 32) out.address = (out.address << 1) | 0UL;
-      else if(out.nbits < 40) out.mode = (out.mode << 1) | 0UL;
-      else if(out.nbits < 64) out.rgb = (out.rgb << 1) | 0UL;
-      else if(out.nbits < 72) out.function = (out.function << 1) | 0UL;
-      else if(out.nbits < 80) out.white = (out.white << 1) | 0UL;
-      else if(out.nbits < 88) out.speed = (out.speed << 1) | 0UL;
-      else out.crc = (out.crc << 1) | 0UL;
+      byte = (byte << 1) | 0;
     } else if (src.expect_mark(FOOTER_MARK_US)) {
       ESP_LOGD(TAG, "Detected Footer Mark");
-      return out;
+      return out.reverse();
     } else {
       return {};
     }
-  }
+
+    if (out.nbits % 8 == 0) {
+      if (out.nbits < 32) {
+      out.address = (out.address << 8) | reverse_bits(byte);
+      } else if (out.nbits < 40) {
+        out.mode = (out.mode << 8) | reverse_bits(byte);
+      } else if (out.nbits < 64) {  
+        out.rgb = (out.rgb << 8) | reverse_bits(byte);
+      } else if (out.nbits < 72) {
+        out.function = (out.function << 8) | reverse_bits(byte);
+      } else if (out.nbits < 80) {
+        out.white = (out.white << 8) | reverse_bits(byte);
+      } else if (out.nbits < 88) {
+        out.speed = (out.speed << 8) | reverse_bits(byte);
+      } else if (out.nbits < 104) {
+        out.crc = (out.crc << 8) | reverse_bits(byte);
+      }
+    }
   ESP_LOGD(TAG, "End of Data");
-  return out;
+  return outreverse();
 }
 void LTECHProtocol::dump(const LTECHData &data) {
-  ESP_LOGI(TAG, "Received LTECH address: %08" PRIX32 ", mode: %02" PRIX32 ", rgb: %06" PRIX32 ", function: %02" PRIX32 ", white: %02" PRIX32 ", speed: %02" PRIX32 ", crc: %04" PRIX32 ", nbits=%d", rev8Bit(data.address), rev8Bit(data.mode), rev8Bit(data.rgb) , rev8Bit(data.function), rev8Bit(data.white), rev8Bit(data.speed), rev8Bit(data.crc), data.nbits );
+  ESP_LOGI(TAG, "Received LTECH address: %08" PRIX32 ", mode: %02" PRIX32 ", rgb: %06" PRIX32 ", function: %02" PRIX32 ", white: %02" PRIX32 ", speed: %02" PRIX32 ", crc: %04" PRIX32 ", nbits=%d", data.address, data.mode, data.rgb, data.function, data.white, data.speed, data.crc, data.nbits );
 } 
 }  // namespace remote_base
 } 
