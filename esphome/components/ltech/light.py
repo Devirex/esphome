f03516836325f99a800c47e5328021bf54505dae
Devirex <naibaf_11@yahoo.de>
2024-08-01 16:42:53 +0200

    Added Ltech

---
+++ esphome/components/ltech/light.py
@@ -0,0 +1,34 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import light, remote_base
+from esphome.const import (
+    CONF_BLUE,
+    CONF_COLOR_INTERLOCK,
+    CONF_GREEN,
+    CONF_RED,
+    CONF_OUTPUT_ID,
+    CONF_WHITE,
+    CONF_ADDRESS,
+)
+
+ltech_ns = cg.esphome_ns.namespace("ltech")
+LTECHLightOutput = ltech_ns.class_("LTECHLightOutput", light.LightOutput)
+
+CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
+    {
+        cv.Required(CONF_ADDRESS): cv.hex_uint32_t,
+        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(LTECHLightOutput),
+        #cv.Required(CONF_RED): cv.use_id(output.FloatOutput),
+        #cv.Required(CONF_GREEN): cv.use_id(output.FloatOutput),
+        #cv.Required(CONF_BLUE): cv.use_id(output.FloatOutput),
+        #cv.Required(CONF_WHITE): cv.use_id(output.FloatOutput),
+        #cv.Optional(CONF_COLOR_INTERLOCK, default=False): cv.boolean,
+    }
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    await light.register_light(var, config)
+    address = await cg.get_variable(config[CONF_ADDRESS])
+    cg.add(var.set_address(address))
---
+++ esphome/components/ltech/ltech_light_output.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/output/float_output.h"
+#include "esphome/components/light/light_output.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace ltech {
+
+class LTECHLightOutput : public light::LightOutput {
+ public:
+  void set_address(uint32_t address) { address_ = address; }
+  void set_color_interlock(bool color_interlock) { color_interlock_ = color_interlock; }
+  light::LightTraits get_traits() override {
+    auto traits = light::LightTraits();
+    if (this->color_interlock_)
+      traits.set_supported_color_modes({light::ColorMode::RGB, light::ColorMode::WHITE});
+    else
+      traits.set_supported_color_modes({light::ColorMode::RGB_WHITE});
+    return traits;
+  }
+  void write_state(light::LightState *state) override {
+    //this->red_->set_level(red);
+    //this->green_->set_level(green);
+    //this->blue_->set_level(blue);
+    //this->white_->set_level(white);
+  }
+
+  void update_state(light::LightState *state) override {
+    float red, green, blue, white;
+    state->current_values_as_rgbw(&red, &green, &blue, &white, this->color_interlock_);
+    ESP_LOGVV(TAG, "RGBW: %.2f, %.2f, %.2f, %.2f", red, green, blue, white);
+    //this->red_->set_level(red);
+    //this->green_->set_level(green);
+    //this->blue_->set_level(blue);
+    //this->white_->set_level(white);
+  }
+
+ protected:
+  uint32_t *address_;
+  bool color_interlock_{false};
+};
+
+}  // namespace rgbw
+}  // namespace esphome
