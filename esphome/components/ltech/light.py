9dcd4e446858c8889d166acd15b576e57d520dec
Devirex <naibaf_11@yahoo.de>
2024-08-01 19:49:42 +0200

    Added Ltech

--- esphome/components/ltech/light.py
+++ esphome/components/ltech/light.py
@@ -12,11 +12,11 @@ from esphome.const import (
 )
 
 ltech_ns = cg.esphome_ns.namespace("ltech")
-LTECHLightOutput = ltech_ns.class_("LTECHLightOutput", light.LightOutput)
+#LTECHLightOutput = ltech_ns.class_("LTECHLightOutput", light.LightOutput)
 
 CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
     {
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(LTECHLightOutput),
+        #cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(LTECHLightOutput),
         cv.Required(CONF_OUTPUT): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
         #cv.Required(CONF_RED): cv.use_id(output.FloatOutput),
         #cv.Required(CONF_GREEN): cv.use_id(output.FloatOutput),
@@ -28,8 +28,8 @@ CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
-    await light.register_light(var, config)
+    #var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    #await light.register_light(var, config)
 
     transmitter = await cg.get_variable(config[CONF_OUTPUT])
     cg.add(var.set_transmitter(transmitter))
